ab255aea2576d0fb40b40badaca2c452506074a3
Dp-052004 <121806338+Dp-052004@users.noreply.github.com>
2026-01-09 09:20:19 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class First {
+	public static void main(String[] args) {
+		String name="Diptyanil Basak";
+		String PhoneNo= "9007366772";
+		String gender="Male";
+		boolean married=false;
+		int age=21;
+		double weight=69.5;
+		
+		System.out.println(name);
+		System.out.println(PhoneNo);
+		System.out.println(gender);
+		System.out.println(married);
+		System.out.println(age);
+		System.out.println(weight);
+	}	
+}
---
+++ Second.java
@@ -0,0 +1,32 @@
+package assignment;
+
+public class Second {
+	public static void main(String[] args) {
+		int a=2;
+		int b=4;
+		float c=3.2f;
+		String s="abc";
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a+c);
+		System.out.println(a-c);
+		System.out.println(a*c);
+		System.out.println(a/c);
+		System.out.println(a%c);
+		System.out.println(a+s);
+		System.out.println(s+a);
+		System.out.println(s+c);
+		System.out.println(s+a+s);
+		
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+	}
+	
+}
